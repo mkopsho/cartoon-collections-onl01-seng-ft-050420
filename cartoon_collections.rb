6465cce05316e34c0ffe110e38f546046abb5efc
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-06 21:10:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |cheese|
-    cheese_types.include?(cheese)
+  array.find do |item|
+    cheese_types.include?(item)
   end
 end
