e6525bfdd78553e915fdffdc1c60bd8bcd013d8e
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-06 21:05:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |call|
-    if call.length > 4
+  array.any? do |call|
+    call.length > 4
 end
 
 def find_the_cheese# code an argument here
